7d1d7066e7b392301c768ca9fb591fbfc54169a1
Oliver <ofcharrison@gmail.com>
2019-03-18 15:45:38 +0000

    stores note in an array

--- spec/notelistmodelSpec.js
+++ spec/notelistmodelSpec.js
@@ -7,3 +7,17 @@
   };
   notelistTest();
 })(this);
+
+(function(exports) {
+  function notelistarrayTest() {
+    var noteapp = new NoteApp("Hi Bob")
+    var notelist = new NoteList();
+    var notelist_array = notelist.list;
+
+    noteapp.storearray(notelist_array)
+    // console.log(notelist.list)
+    assert.isTrue(notelist.list[0] == "Hi Bob");
+
+  };
+  notelistarrayTest();
+})(this);
--- src/note-model.js
+++ src/note-model.js
@@ -1,10 +1,14 @@
 (function(exports) {
   function NoteApp(text){
-    this.text = text
+    this.text = text;
   };
 
   NoteApp.prototype.exclaim = function(text){
     return text
+  };
+
+  NoteApp.prototype.storearray = function(notelist_array) {
+    notelist_array.push(this.text)
   };
   exports.NoteApp = NoteApp;
 })(this);
